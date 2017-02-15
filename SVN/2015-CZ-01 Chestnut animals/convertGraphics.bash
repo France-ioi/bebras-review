4326c5ecb5eaefcc4d59c036f659df1184199076
Mathias Hiron <mathias.hiron@gmail.com>
2017-02-15 15:47:04 +0100

    delete content of SVN (not part of the project)

--- SVN/2015-CZ-01 Chestnut animals/convertGraphics.bash
+++
@@ -1,9 +0,0 @@
-# Use this with Linux and inkscape installed
-
-for svg in `ls *.svg`
-do
-    echo "Making png of" $svg
-    inkscape -D --export-dpi=100 --export-png=${svg:0:-3}png $svg
-done
-
-
--- SVN/2015-SE-01-EN-Stars/readme.txt
+++
@@ -1,2 +0,0 @@
-Take care: The .odt-file is created using LibreOffice.
-Alas, it will not open in OpenOffice.
