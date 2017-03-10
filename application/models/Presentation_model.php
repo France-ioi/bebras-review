db806aea8924cc076052fa3b07995ad71f7610e9
Michel Blockelet <blockelet@crans.org>
2017-03-10 10:17:51 +0100

    Actually implement SVN update, adapt display

--- application/models/Presentation_model.php
+++ application/models/Presentation_model.php
@@ -21,7 +21,11 @@ class presentation_model extends CI_Model {
 		$result = $tasks->result_array();
 		for($i=0;$i<$tasks->num_rows();$i++)
 		{
-			$result[$i]['ownerName']=$userID_to_name[$result[$i]['ownerID']];
+            if($result[$i]['ownerID'] == -1) {
+    			$result[$i]['ownerName'] = $result[$i]['svnLogin'];
+            } else {
+    			$result[$i]['ownerName'] = $userID_to_name[$result[$i]['ownerID']];
+            }
 			if($result[$i]['assignedGroupID']!="0")
 			{
 				$group = $this->db->get_where('groups',array('ID'=>$result[$i]['assignedGroupID']))->result_array();
@@ -193,7 +197,7 @@ class presentation_model extends CI_Model {
 	{
 		$item=$_POST['data'];
 
-		$this->db->update('tasks',array('htmlFilename'=>$item['htmlFilename'],'pdfFileName'=>$item['pdfFileName'],'odtFileName'=>$item['odtFileName'],'assignedGroupID'=>$item['assignedGroupID'],'status'=>$item['status'],'statusComment'=>$item['statusComment'],'ownerComment'=>$item['ownerComment']),array('ID'=>$item['ID']));
+		$this->db->update('tasks',array('htmlFileName'=>$item['htmlFileName'],'pdfFileName'=>$item['pdfFileName'],'odtFileName'=>$item['odtFileName'],'assignedGroupID'=>$item['assignedGroupID'],'status'=>$item['status'],'statusComment'=>$item['statusComment'],'ownerComment'=>$item['ownerComment']),array('ID'=>$item['ID']));
 
 		return $item;
 	}
@@ -359,9 +363,9 @@ class presentation_model extends CI_Model {
 		$result['localCheckoutFolder']=$users[0]['localCheckoutFolder'];
 		for($i=0;$i<$tasks->num_rows();$i++)
 		{
-			$result[$i]['link']="/bebras-review/SVN/".$result[$i]['folderName']."/".$result[$i]['htmlFilename'];
-			$result[$i]['otherlink']=$users[0]['localCheckoutFolder']."/".$result[$i]['folderName']."/".$result[$i]['htmlFilename'];
-			if($result[$i]['htmlFilename']==NULL)
+			$result[$i]['link'] = $this->config->item('svn_reldir') . $result[$i]['folderPath'] . '/' . $result[$i]['folderName'] . '/' . $result[$i]['htmlFileName'];
+			$result[$i]['otherlink']=$users[0]['localCheckoutFolder']."/".$result[$i]['folderName']."/".$result[$i]['htmlFileName'];
+			if($result[$i]['htmlFileName']==NULL)
 			{
 				$result[$i]['link']="";
 				$result[$i]['otherlink']="";
@@ -381,7 +385,7 @@ class presentation_model extends CI_Model {
 		$result['localCheckoutFolder']=$users[0]['localCheckoutFolder'];
 		for($i=0;$i<$tasks->num_rows();$i++)
 		{
-			$result[$i]['link']="/bebras-review/SVN/".$result[$i]['folderName']."/".$result[$i]['pdfFileName'];
+			$result[$i]['link'] = $this->config->item('svn_reldir') . $result[$i]['folderPath'] . '/' . $result[$i]['folderName'] . '/' . $result[$i]['pdfFileName'];
 			$result[$i]['otherlink']=$users[0]['localCheckoutFolder']."/".$result[$i]['folderName']."/".$result[$i]['pdfFileName'];
 			if($result[$i]['pdfFileName']==NULL)
 			{
@@ -403,7 +407,7 @@ class presentation_model extends CI_Model {
 		$result['localCheckoutFolder']=$users[0]['localCheckoutFolder'];
 		for($i=0;$i<$tasks->num_rows();$i++)
 		{
-			$result[$i]['link']="/bebras-review/SVN/".$result[$i]['folderName']."/".$result[$i]['odtFileName'];
+			$result[$i]['link'] = $this->config->item('svn_reldir') . $result[$i]['folderPath'] . '/' . $result[$i]['folderName'] . '/' . $result[$i]['odtFileName'];
 			$result[$i]['otherlink']=$users[0]['localCheckoutFolder']."/".$result[$i]['folderName']."/".$result[$i]['odtFileName'];
 			
 			if($result[$i]['odtFileName']==NULL)
@@ -422,84 +426,92 @@ class presentation_model extends CI_Model {
 		$this->db->update('users',array('autoLoadTasks'=>"true"),array('username'=>$username)); // TODO :: uh, always true?
 	}
 
-	public function svnlist($repository)
-	{
-		$checkout = 'svn list "'.$repository.'"';
-		exec($checkout,$a);
-		$co=count($a);
-		$i=0;
-		for($i=0;$i<$co;$i++)
-		{
-			$item=array();
-			if(substr($a[$i],-1)=="/"&&is_numeric(substr($a[$i],0,4))&&substr($a[$i],4,1)=="-"&&substr($a[$i],7,1)=="-"&&is_numeric(substr($a[$i],8,2)))
-			{
-				$htm=array();
-				$item["folderName"]=substr($a[$i],0,-1);
-				$textID=substr($a[$i],0,10);
-				$item["textID"]=$textID;
-				$item["year"]=substr($a[$i],0,4);
-				$item["countryCode"]=substr($a[$i],5,2);
-				$checkout = 'svn info "'.$repository.$a[$i].'"';
-				exec($checkout,$b);
-				$item['repositoryDate']=substr($b[9],19,10);
-				$item['importDate']=date('y-m-d');
-				$item['lastChangeDate']=date('y-m-d');
-				$item['svnLogin']="svnLogin";
-				$item['ownerID']="1";
-				//htmlFileName
-				$checkout = 'svn list "'.$repository.$a[$i].'"';
-				exec($checkout,$htm);
-				$cohtml=count($htm);
-				for($j=0;$j<$cohtml;$j++)
-				{
-					if($htm[$j]=="index.html")
-						break;
-					else if(strlen($htm[$j])>=15&&substr($htm[$j],0,10)==$textID&&substr($htm[$j],-5)==".html")
-						break;
-				}
-				if($j<$cohtml)
-					$item['htmlFilename']=$htm[$j];
-				//odtFileName
-				for($j=0;$j<$cohtml;$j++)
-				{
-					if(strlen($htm[$j])>=14&&substr($htm[$j],0,10)==$textID&&substr($htm[$j],-4)==".odt")
-						break;
-				} 
-				if($j<$cohtml)
-					$item['odtFileName']=$htm[$j];
-				//pdfFileName
-				for($j=0;$j<$cohtml;$j++)
-				{
-					if(strlen($htm[$j])>=14&&substr($htm[$j],0,10)==$textID&&substr($htm[$j],-4)==".pdf")
-						break;
-				} 
-				if($j<$cohtml)
-					$item['pdfFileName']=$htm[$j];
-				
-				$check = $this->db->get_where('tasks',array('folderName'=>$item["folderName"]));
-				$checkflag = $check->num_rows();
-				if($checkflag==0)
-					$this->db->insert('tasks',$item);
-				else
-				{
-					$this->db->update('tasks',array('htmlFilename'=>$item['htmlFilename'],'pdfFileName'=>$item['pdfFileName'],'odtFileName'=>$item['odtFileName'],'lastChangeDate'=>date('y-m-d')),array('folderName'=>$item["folderName"]));
-				}
+    public function recupdatesvn($baseDir, $dir) {
+        $dirFullPath = $baseDir . $dir;
+        $dirName = basename($dirFullPath);
+        // Check whether it's a task folder
+        if(strlen($dirName) > 11 && $dirName[4] == '-' && $dirName[7] == '-') {
+            // Current folder is a task folder
+            $newItem = array();
 
-			}
-			else if(substr($a[$i],-1)=="/")
+            $newItem['folderName'] = $dirName;
+            $newItem['folderPath'] = dirname($dir);
+
+            $newItem['year'] = substr($dirName, 0, 4);
+            $newItem['countryCode'] = substr($dirName, 5, 2);
+            $newItem['textID'] = substr($dirName, 0, 10);
+
+            $newItem['importDate'] = date('y-m-d');
+
+            $svnLogs = svn_log($dirFullPath); // Can't do better
+            $newItem['svnLogin'] = end($svnLogs)['author'];
+            $newItem['repositoryDate'] = substr(end($svnLogs)['date'], 0, 10);
+            $newItem['lastChangeDate'] = substr($svnLogs[0]['date'], 0, 10);
+
+		    $owners = $this->db->get_where('users', array('svnLogin' => $newItem['svnLogin']));
+            if($owners->num_rows() > 0) {
+                $newItem['ownerID'] = $owners->result_array()[0]['ID'];
+            } else {
+                $newItem['ownerID'] = -1;
+                // TODO :: check for new user <-> svnLogin matches after a new svnLogin is given in profile
+            }
+
+            // Search for files; they must start by textID
+            $newItem['htmlFileName'] = '';
+            $newItem['odtFileName'] = '';
+            $newItem['pdfFileName'] = '';
+
+            foreach(scandir($dirFullPath) as $elem) {
+                if(is_dir($dirFullPath.'/'.$elem)) {
+                    continue;
+                } elseif($elem == 'index.html') {
+                    $newItem['htmlFileName'] = $elem;
+                } elseif(substr($elem, 0, 10) == $newItem['textID']) {
+                    foreach(array('html', 'odt', 'pdf') as $ext) {
+                        // We look for the first file with the expected filename
+                        if($newItem[$ext.'FileName'] == '' && substr($elem, -strlen($ext)-1) == '.'.$ext) {
+                            $newItem[$ext.'FileName'] = $elem;
+                        }
+                    }
+                }
+            }
+
+			$check = $this->db->get_where('tasks',array('folderName' => $newItem["folderName"]));
+			if($check->num_rows() == 0)
+				$this->db->insert('tasks', $newItem);
+			else
 			{
-				$this->svnlist($repository.$a[$i]);
+				$this->db->update('tasks',
+                    array('htmlFileName' => $newItem['htmlFileName'],
+                          'pdfFileName' => $newItem['pdfFileName'],
+                          'odtFileName' => $newItem['odtFileName'],
+                          'lastChangeDate' => $newItem['lastChangeDate']),
+                    array('folderName' => $newItem["folderName"]));
 			}
-		}
-	}
-	
+        } else {
+            // Current folder is not a task folder
+            foreach(scandir($dirFullPath) as $elem) {
+                $elemPath = $dirFullPath . '/' . $elem;
+                if(is_dir($elemPath) && $elem != '.' && $elem != '..') {
+                    $this->recupdatesvn($baseDir, $dir . '/' . $elem);
+                }
+            }
+        }
+    }
+
 	public function updatesvn()
-	{		
+	{
 		ini_set('max_execution_time', 3600);
 		
-		$repository = "svn://svn.france-ioi.org/beaver_review_sample/2015/";
-		$result=$this->svnlist($repository);
-		return count($result);
+        svn_auth_set_parameter(SVN_AUTH_PARAM_DEFAULT_USERNAME,             $this->config->item('svn_user'));
+        svn_auth_set_parameter(SVN_AUTH_PARAM_DEFAULT_PASSWORD,             $this->config->item('svn_password'));
+        svn_auth_set_parameter(PHP_SVN_AUTH_PARAM_IGNORE_SSL_VERIFY_ERRORS, true); // <--- Important for certificate issues!
+        svn_auth_set_parameter(SVN_AUTH_PARAM_NON_INTERACTIVE,              true);
+        svn_auth_set_parameter(SVN_AUTH_PARAM_NO_AUTH_CACHE,                true);
+
+        foreach($this->config->item('svn_subdirs') as $subdir) {
+            $newRev = svn_update($this->config->item('svn_basedir') + $subdir);
+            $this->recupdatesvn($this->config->item('svn_basedir'), $subdir);
+        }
 	}
 }
-
